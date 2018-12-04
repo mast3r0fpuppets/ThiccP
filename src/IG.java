79043f0acd0e433f7ea4aa24f6822ee895b0c821
mast3r0fpuppets <mast3r0fpuppets@users.noreply.github.com>
2018-12-04 15:39:17 +0000

    Disconnect

--- src/IG.java
+++ src/IG.java
@@ -3,6 +3,9 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,15 +16,17 @@ import javax.swing.JProgressBar;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
-public class IG {
+public class IG extends Thread {
 	private JFrame frame;
-
-	public IG() {
+	private static Utilizador u;
+	
+	public IG() throws NumberFormatException, IOException {
 		frame = new JFrame("Interface Grafica");
 		frame.setLayout(new BorderLayout());
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		addFrameContent();
 		frame.pack();
+		
 	}
 
 	public void open(int i, int j) {
@@ -50,7 +55,7 @@ public class IG {
 		frame.add(partecima, BorderLayout.NORTH);
 		// LADO DIREITO
 		JPanel ladodireito = new JPanel();
-		ladodireito.setLayout(new GridLayout(2, 1));
+		ladodireito.setLayout(new GridLayout(4, 2));
 		JButton descarregar = new JButton("Descarregar");
 		descarregar.addActionListener(new ActionListener() {
 
@@ -59,6 +64,14 @@ public class IG {
 			}
 		});
 		ladodireito.add(descarregar);
+		JButton disconnect = new JButton("Disconnect");
+		disconnect.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				u.Exit();
+			}
+		});
+		ladodireito.add(disconnect);
 		JProgressBar prog = new JProgressBar();
 		ladodireito.add(prog);
 		frame.add(ladodireito, BorderLayout.EAST);
@@ -67,8 +80,11 @@ public class IG {
 		frame.add(list, BorderLayout.CENTER);
 	}
 
-	public static void main(String args[]) {
+	public static void main(String args[]) throws NumberFormatException, IOException {
+		u = new Utilizador();
 		IG window = new IG();
-		window.open(350,350);
+		window.open(350, 350);
+		u.Connect("8080", "234");
+
 	}
 }
